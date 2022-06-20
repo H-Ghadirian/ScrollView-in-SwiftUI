f8f5c617b98070c00818fa95cdd664cc3bc41366
Hamed Ghadirian <H.qadirian@gmail.com>
2022-06-20 12:10:27 +0200

    Initial Commit

---
+++ Swiftui scrollview/Swiftui_scrollviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftui_scrollviewApp.swift
+//  Swiftui scrollview
+//
+//  Created by Ghadirian, Hamed, HSE24 DE on 20.06.22.
+//
+
+import SwiftUI
+
+@main
+struct Swiftui_scrollviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
