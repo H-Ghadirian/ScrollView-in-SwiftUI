a5e38c5466937e3de152ecbef05005a41e8f09b7
Hamed Ghadirian <h.ghadirian@outlook.com>
2022-06-23 18:29:27 +0200

    Add GeometryReader and DragGesture to detect scrolling

--- Swiftui scrollview/ContentView.swift
+++ Swiftui scrollview/ContentView.swift
@@ -6,15 +6,36 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView {
+            GeometryReader { geometry in
+                Rectangle()
+                    .frame(width: 0, height: 0)
+                    .onChange(
+                        of: geometry.frame(in: .global).midY) { midY in
+                            print("midY: \(midY)")
+                        }
+            }
+            VStack {
+                ForEach(0..<50) { index in
+                    Rectangle()
+                        .frame(width: 300, height: 50)
+                        .foregroundColor(.accentColor)
+                }
+            }
         }
+        .gesture(
+            DragGesture()
+                .onChanged { value in
+                    print("Scroll")
+                    if value.translation.height > 0 {
+                        print("Scroll up")
+                    } else {
+                        print("Scroll down")
+                    }
+                }
+        )
     }
 }
 
